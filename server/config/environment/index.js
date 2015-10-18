fa9688671e33cd76f30038233d933f952b623457
flyntde <dflyntgit@gmail.com>
2015-10-18 17:13:45 -0400

    add app-config component.

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -2,52 +2,53 @@
 
 var path = require('path');
 var _ = require('lodash');
+var pjson = require('../../../package.json');
 
 function requiredProcessEnv(name) {
-  if(!process.env[name]) {
-    throw new Error('You must set the ' + name + ' environment variable');
-  }
-  return process.env[name];
+    if(!process.env[name]) {
+        throw new Error('You must set the ' + name + ' environment variable');
+    }
+    return process.env[name];
 }
 
 // All configurations will extend these options
 // ============================================
 var all = {
-  env: process.env.NODE_ENV,
+    env: process.env.NODE_ENV,
 
-  // Root path of server
-  root: path.normalize(__dirname + '/../../..'),
+    // Root path of server
+    root: path.normalize(__dirname + '/../../..'),
 
-  // Server port
-  port: process.env.PORT || 9000,
+    // Server port
+    port: process.env.PORT || 9000,
 
-  // Server IP
-  ip: process.env.IP || '0.0.0.0',
+    // Server IP
+    ip: process.env.IP || '0.0.0.0',
 
-  // Should we populate the DB with sample data?
-  seedDB: false,
+    // Should we populate the DB with sample data?
+    seedDB: false,
 
-  // Secret for session, you will want to change this and make it an environment variable
-  secrets: {
-    session: 'digentity-graph-secret'
-  },
+    // Secret for session, you will want to change this and make it an environment variable
+    secrets: {
+        session: 'digentity-graph-secret'
+    },
 
-  // List of user roles
-  userRoles: ['guest', 'user', 'admin'],
+    // List of user roles
+    userRoles: ['guest', 'user', 'admin'],
 
-  // MongoDB connection options
-  mongo: {
-    options: {
-      db: {
-        safe: true
-      }
-    }
-  },
+    appVersion: pjson.version,
 
+    euiServerUrl: process.env.EUI_SERVER_URL || 'http://localhost',
+    euiServer: process.env.EUI_SERVER || 'localhost',
+    euiServerPort: process.env.EUI_SERVER_PORT || 9200,
+    dbServerUser: process.env.DB_SERVER_USER,
+    dbServerPass: process.env.DB_SERVER_PASS,
+    euiSearchIndex: process.env.EUI_SEARCH_INDEX || 'dig-latest',
+    euiSearchType: process.env.EUI_SEARCH_TYPE || 'WebPage',
 };
 
 // Export the config object based on the NODE_ENV
 // ==============================================
 module.exports = _.merge(
-  all,
-  require('./' + process.env.NODE_ENV + '.js') || {});
+    all,
+    require('./' + process.env.NODE_ENV + '.js') || {});
--- server/routes.js
+++ server/routes.js
@@ -1,24 +1,33 @@
 /**
- * Main application routes
- */
+* Main application routes
+*/
 
 'use strict';
 
-var errors = require('./components/errors');
-var path = require('path');
+var errors = require('./components/errors'),
+config = require('./config/environment'),
+path = require('path');
 
 module.exports = function(app) {
 
-  // Insert routes below
-  app.use('/api/things', require('./api/thing'));
-  
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+    // Insert routes below
+    app.use('/api/things', require('./api/thing'));
 
-  // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+    app.get('/config/?', function(req, res) {
+        res.status(200).send({
+            euiHost: config.euiServerUrl,
+            euiPort: config.euiServerPort,
+            euiSearchIndex: config.euiSearchIndex,
+            appVersion: config.appVersion       
+        });
+    });
+
+    // All undefined asset or api routes should return a 404
+    app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+    .get(errors[404]);
+
+    // All other routes should redirect to the index.html
+    app.route('/*').get(function(req, res) {
+        res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
     });
 };
