076bf63b798d5ab6d20b7180561d6cecb7725516
flyntde <dflyntgit@gmail.com>
2016-05-09 15:19:35 -0400

    refactor changes necessary for update to elastic-type-selector

---
+++ server/config/elastic-types.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+
+    TYPE_MAP_ARRAY: [
+    {displayName:'ALL', typeName: '-'},
+    {displayName:'email', typeName: 'email'},
+    {displayName:'phone', typeName: 'phone'},
+    {displayName:'seller', typeName: 'seller'},
+    {displayName:'provider', typeName: 'adultservice'},
+    {displayName:'webpage', typeName: 'webpage'},
+    {displayName:'offer', typeName: 'offer'}
+    ]
+};
