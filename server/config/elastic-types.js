14e491311eb0f28529fd62fe4d88587ac327f3d6
flyntde <dflyntgit@gmail.com>
2016-05-12 16:36:21 -0400

    only show webpage type

--- server/config/elastic-types.js
+++ server/config/elastic-types.js
@@ -3,12 +3,6 @@
 module.exports = {
 
     TYPE_MAP_ARRAY: [
-    {displayName:'ALL', typeName: '-'},
-    {displayName:'email', typeName: 'email'},
-    {displayName:'phone', typeName: 'phone'},
-    {displayName:'seller', typeName: 'seller'},
-    {displayName:'provider', typeName: 'adultservice'},
-    {displayName:'webpage', typeName: 'webpage'},
-    {displayName:'offer', typeName: 'offer'}
+    {displayName:'webpage', typeName: 'webpage'}
     ]
 };
