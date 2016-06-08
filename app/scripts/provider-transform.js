7c0d399be7068f9e29cf9e9f15562e98a5cca3f7
Rachel Artiss <rachel.artiss@nextcentury.com>
2016-06-08 16:54:02 -0400

    Fixing jshint issues.

--- app/scripts/provider-transform.js
+++ app/scripts/provider-transform.js
@@ -2,41 +2,41 @@
  * transform elastic search webpage query to display format.  See data-model.json
  */
 
-/* globals _, commonTransforms */
+/* globals _ */
 /* exported providerTransform */
 /* jshint camelcase:false */
 
-/* note lodash should be defined in parent scope, as well as commonTransforms */
-var providerTransform = (function(_, commonTransforms) {
+/* note lodash should be defined in parent scope */
+var providerTransform = (function(_) {
 
     function getProvider(record) {
         var person = {};
         person._id = _.get(record, 'uri');
-        person._type = "provider";
+        person._type = 'provider';
         person.name = _.get(record, 'name', 'Name N/A');
         person.ethnicities = _.get(record, 'ethnicity');
         person.height = _.get(record, 'height');
         person.weight = _.get(record, 'weight');
         person.ages = _.get(record, 'age');
 
-        var title = (person.name != 'Name N/A')? person.name : "";
-        var sep = (title == "")? "": ", ";
+        var title = (person.name !== 'Name N/A')? person.name : '';
+        var sep = (title === '')? '': ', ';
         if(person.ages) {
-            title += sep + person.ages[0]
-            sep = ", "
+            title += sep + person.ages[0];
+            sep = ', ';
         }
         if(person.ethnicities) {
-            title += sep + person.ethnicities[0]
-            sep = ", "
+            title += sep + person.ethnicities[0];
+            sep = ', ';
         }
         person.title = title;
         person.sellers = [];
-        var offers = _.get(record, 'offers')
+        var offers = _.get(record, 'offers');
         if(offers) {
             offers.forEach(function(offer) {
-                var seller = _.get(offer, "seller");
+                var seller = _.get(offer, 'seller');
                 if(seller) {
-                    person.sellers.push(_.get(seller, "uri"))
+                    person.sellers.push(_.get(seller, 'uri'));
                 }
             });
         }
@@ -56,4 +56,4 @@ var providerTransform = (function(_, commonTransforms) {
         }
     };
 
-})(_, commonTransforms);
+})(_);
