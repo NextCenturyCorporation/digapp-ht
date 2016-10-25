8aca1f79b7bd501794d8f6fe6c3cc03bb4ca5362
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2016-10-25 16:42:15 -0400

    DIG-909 DIG-910 The state parameter in the browser URL is no longer updated whenever a link is generated through the filter-state-manager component.  Additionally, the state parameter is removed on page load once the filter state is updated in order to avoid user confusion since the state parameter does not continue to update.

--- app/behaviors/browser-behavior.js
+++ app/behaviors/browser-behavior.js
@@ -1,4 +1,4 @@
-/* globals DigBehaviors */
+/* globals _, DigBehaviors */
 /* exported DigBehaviors */
 var DigBehaviors = DigBehaviors || {};
 
@@ -6,6 +6,22 @@ var DigBehaviors = DigBehaviors || {};
  * Polymer behavior for browser-related utility functions.
  */
 DigBehaviors.BrowserBehavior = {
+  /**
+   * Returns the page link using the given window.location object with the given state parameter string.
+   */
+  generateLinkWithStateParameter: function(location, state) {
+    var parameters = this.getUrlParameters(location.search);
+    if(state) {
+      parameters.state = state;
+    } else {
+      delete parameters.state;
+    }
+    var parameterStrings = _.keys(parameters).map(function(key) {
+      return key + '=' + parameters[key];
+    });
+    return location.protocol + '//' + location.host + location.pathname + (parameterStrings.length ? '?' + parameterStrings.join('&') : '');
+  },
+
   /**
    * Returns the URL parameters from the given string.
    */
@@ -18,5 +34,14 @@ DigBehaviors.BrowserBehavior = {
       });
     }
     return parameters;
+  },
+
+  /**
+   * Removes the state parameter from the browser URL using the given window.history and window.location objects.
+   */
+  removeStateParameter: function(history, location) {
+    var link = this.generateLinkWithStateParameter(location);
+    // Update the browser URL without reloading the page.
+    history.pushState({}, '', link);
   }
 };
