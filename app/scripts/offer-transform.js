62d2cdc82ac604a7a603fb0bc72243b979bc9967
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2016-12-13 14:44:11 -0500

    DIG-924 Prototype of adding annotation flags to emails and phones in single-records.  Not functional.  WIP.

--- app/scripts/offer-transform.js
+++ app/scripts/offer-transform.js
@@ -65,7 +65,8 @@ var offerTransform = (function(_, commonTransforms, providerTransforms) {
         text: text,
         icon: commonTransforms.getIronIcon(type),
         link: commonTransforms.getLink(uri, type),
-        styleClass: commonTransforms.getStyleClass(type)
+        styleClass: commonTransforms.getStyleClass(type),
+        annotate: true
       };
     });
   }
