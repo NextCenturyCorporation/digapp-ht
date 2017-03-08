dd164d0425b97ff585996b79eb937a8d6cb91927
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2017-03-08 09:40:33 -0500

    Adding image queries to single-records.

--- app/scripts/image-transform.js
+++ app/scripts/image-transform.js
@@ -23,42 +23,14 @@
 
 var imageTransform = (function(_, commonTransforms) {
   return {
-    // expected data is from an elasticsearch
-    image: function(data) {
-      var newData = {};
-      if(data && data.hits.hits.length > 0) {
-        newData = data.hits.hits[0]._source;
-        newData.url = _.isArray(newData.url) ? newData.url[0] : newData.url;
-
-        var adultService = {
-          total: newData.isImagePartOf.length,
-          array: []
-        };
-
-        newData.isImagePartOf = _.isArray(newData.isImagePartOf) ? newData.isImagePartOf : [newData.isImagePartOf];
-        newData.isImagePartOf.forEach(function(service) {
-          if(service.mainEntity) {
-            adultService.array.push(service.mainEntity.itemOffered.uri);
-          }
-        });
-
-        newData.adultService = adultService;
-      }
-      newData.id = newData.uri;
-      return newData;
-    },
-
     images: function(data) {
       var images = [];
-      if(data) {
-        data.hits.hits.forEach(function(hit) {
-          var imageId = _.get(hit._source, 'uri', '');
-          var imageSource = _.get(hit._source, 'url', '');
+      if(data && data.aggregations && data.aggregations.image && data.aggregations.image.image && data.aggregations.image.image.buckets) {
+        data.aggregations.image.image.buckets.forEach(function(bucket) {
           images.push({
-            id: imageId,
             icon: commonTransforms.getIronIcon('image'),
-            link: commonTransforms.getLink(imageId, 'image'),
-            source: _.isArray(imageSource) ? imageSource[0] : imageSource,
+            link: commonTransforms.getLink(bucket.key, 'image'),
+            source: bucket.key,
             styleClass: commonTransforms.getStyleClass('image')
           });
         });
@@ -66,12 +38,8 @@ var imageTransform = (function(_, commonTransforms) {
       return images;
     },
 
-    imageTotal: function(data) {
-      return (data && data.hits) ? data.hits.total : 0;
-    },
-
     externalImageLink: function(id) {
-      return commonTransforms.getLink('http://dig.isi.edu/ht/data/' + id, 'image');
+      return commonTransforms.getLink(id, 'image');
     }
   };
 });
