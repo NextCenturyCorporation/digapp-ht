9bc4bec810c3ac40dd8fb63025f31eb832b1e7e7
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2017-03-23 14:45:49 -0400

    Updated sample server configuration file and default configuration values.

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -1,14 +1,4 @@
-'use strict';
-
-// Use local.env.js for environment variables that grunt will set when the server starts locally.
-// Use for your api keys, secrets, etc. This file should not be tracked by git.
-//
-// You will need to set these on the server you deploy to.
-
 module.exports = {
-  DOMAIN:           'http://localhost:9000',
-  SESSION_SECRET:   'digentitygraph-secret',
-
-  // Control debug level for modules using visionmedia/debug
-  DEBUG: ''
+  ELASTIC_CONFIG: 'http://localhost:9200',
+  ELASTIC_INDEX:  'sampleDataIndexName'
 };
