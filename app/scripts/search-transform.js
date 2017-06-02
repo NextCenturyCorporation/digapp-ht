b9dad83885cd47b7ee9918055001390955d036c6
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2017-06-02 16:40:23 -0400

    DIG-1065 Updated the facets to use the query webservice.

--- app/scripts/search-transform.js
+++ app/scripts/search-transform.js
@@ -17,7 +17,14 @@
 /* exported searchTransform */
 /* jshint camelcase:false */
 
-var searchTransform = (function(_) {
+var searchTransform = (function(_, commonTransforms) {
+  function getAggregationDataFromResponse(response, property) {
+    if(response && response.length && response[0].result && response[0].result.aggregations && response[0].result.aggregations['?' + property]) {
+      return response[0].result.aggregations['?' + property].buckets || [];
+    }
+    return [];
+  }
+
   function getSearchPredicateFromUiType(type) {
     switch(type) {
       case 'eyeColor': return 'eye_color';
@@ -49,8 +56,97 @@ var searchTransform = (function(_) {
     return type;
   }
 
+  function getTemplateFromSearchParameters(searchParameters, optional) {
+    var predicates = {};
+    var template = {
+      clauses: [],
+      filters: [],
+      selects: []
+    };
+
+    if(!_.isEmpty(searchParameters)) {
+      _.keys(searchParameters).forEach(function(type) {
+        var predicate = getSearchPredicateFromUiType(type);
+        _.keys(searchParameters[type]).forEach(function(term) {
+          if(searchParameters[type][term].enabled) {
+            if(type === 'postingDate') {
+              // Use a single date variable ('date1').
+              predicates.date = 1;
+
+              if(template.filters.length === 0) {
+                template.filters.push({});
+              }
+
+              if(!template.filters[0].operator) {
+                template.filters[0].operator = 'and';
+              }
+
+              if(!template.filters[0].clauses) {
+                template.filters[0].clauses = [];
+              }
+
+              template.filters[0].clauses.push({
+                constraint: searchParameters[type][term].date,
+                operator: term === 'dateStart' ? '>' : '<',
+                variable: '?date1'
+              });
+            } else if(predicate) {
+              predicates[predicate] = (predicates[predicate] || 0) + 1;
+
+              template.clauses.push({
+                constraint: searchParameters[type][term].key,
+                isOptional: optional,
+                predicate: predicate
+              });
+            }
+          }
+        });
+      });
+
+      _.keys(predicates).forEach(function(predicate) {
+        for(var i = 1; i <= predicates[predicate]; ++i) {
+          template.selects.push({
+            type: 'simple',
+            variable: '?' + predicate + i
+          });
+          template.clauses.push({
+            isOptional: (predicate === 'date' ? false : optional),
+            predicate: predicate === 'date' ? 'posting_date' : predicate,
+            variable: '?' + predicate + i
+          });
+        }
+      });
+    }
+
+    return template;
+  }
+
   return {
-    result: function(response) {
+    adQuery: function(searchParameters, config) {
+      var template = getTemplateFromSearchParameters(searchParameters, true);
+      var groupBy = (!config || !config.page || !config.pageSize) ? undefined : {
+        limit: config.pageSize,
+        offset: (config.page - 1) * config.pageSize
+      };
+
+      return {
+        SPARQL: {
+          'group-by': groupBy,
+          select: {
+            variables: template.selects
+          },
+          where: {
+            clauses: template.clauses,
+            filters: template.filters,
+            type: 'Ad',
+            variable: '?ad'
+          }
+        },
+        type: 'Point Fact'
+      };
+    },
+
+    adResults: function(response) {
       if(response && response.length && response[0].result) {
         var fields = {};
         if(response[0].query && response[0].query.SPARQL && response[0].query.SPARQL.where && response[0].query.SPARQL.where.clauses && response[0].query.SPARQL.where.clauses.length) {
@@ -64,7 +160,7 @@ var searchTransform = (function(_) {
         }
         return {
           fields: fields,
-          hits: response[0].result.hits
+          hits: response[0].result.hits || []
         };
       }
       return {
@@ -73,87 +169,138 @@ var searchTransform = (function(_) {
       };
     },
 
-    search: function(page, pageSize, searchParameters) {
-      var selects = [];
-      var clauses = [];
-      var filters = [];
-      var groupBy;
-      var predicates = {};
+    facetsQuery: function(searchParameters, config) {
+      var predicate = (config && config.aggregationType ? getSearchPredicateFromUiType(config.aggregationType) : undefined);
+      var template = getTemplateFromSearchParameters(searchParameters, false);
+      var groupBy = {
+        limit: (config && config.pageSize ? config.pageSize : 0),
+        offset: 0
+      };
 
-      if(!_.isEmpty(searchParameters)) {
-        _.keys(searchParameters).forEach(function(type) {
-          var predicate = getSearchPredicateFromUiType(type);
-          _.keys(searchParameters[type]).forEach(function(term) {
-            if(searchParameters[type][term].enabled) {
-              if(type === 'postingDate') {
-                // Use a single date variable ('date1').
-                predicates.date = 1;
-
-                if(filters.length === 0) {
-                  filters.push({});
-                }
-
-                if(!filters[0].operator) {
-                  filters[0].operator = 'and';
-                }
-
-                if(!filters[0].clauses) {
-                  filters[0].clauses = [];
-                }
-
-                filters[0].clauses.push({
-                  constraint: searchParameters[type][term].date,
-                  operator: term === 'dateStart' ? '>' : '<',
-                  variable: '?date1'
-                });
-              } else if(predicate) {
-                predicates[predicate] = (predicates[predicate] || 0) + 1;
-
-                clauses.push({
-                  constraint: searchParameters[type][term].key,
-                  isOptional: true,
-                  predicate: predicate
-                });
-              }
-            }
-          });
+      if(predicate) {
+        template.selects.push({
+          'function': 'count',
+          type: 'function',
+          variable: '?' + predicate
         });
 
-        _.keys(predicates).forEach(function(predicate) {
-          for(var i = 1; i <= predicates[predicate]; ++i) {
-            selects.push({
-              type: 'simple',
-              variable: '?' + predicate + i
-            });
-            clauses.push({
-              isOptional: (predicate !== 'date'),
-              predicate: predicate === 'date' ? 'posting_date' : predicate,
-              variable: '?' + predicate + i
-            });
-          }
+        template.clauses.push({
+          isOptional: false,
+          predicate: predicate,
+          variable: '?' + predicate
         });
 
-        groupBy = (!page || !pageSize) ? undefined : {
-          limit: pageSize,
-          offset: (page - 1) * pageSize
-        };
+        groupBy.variables = [{
+          variable: '?' + predicate
+        }];
       }
 
       return {
         SPARQL: {
           'group-by': groupBy,
           select: {
-            variables: selects
+            variables: template.selects
           },
           where: {
-            clauses: clauses,
-            filters: filters,
+            clauses: template.clauses,
+            filters: template.filters,
             type: 'Ad',
             variable: '?ad'
           }
         },
-        type: 'Point Fact'
+        type: 'Aggregation'
       };
+    },
+
+    cityAggregations: function(response, property) {
+      var data = getAggregationDataFromResponse(response, property);
+      return data.map(function(bucket) {
+        var city = commonTransforms.getLocationDataFromId(bucket.key).city;
+        /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+        return {
+          count: bucket.doc_count,
+          id: city,
+          link: commonTransforms.getLink(bucket.key, 'location'),
+          text: city
+        };
+        /* jscs:enable requireCamelCaseOrUpperCaseIdentifiers */
+      }).filter(function(city) {
+        return city.text;
+      });
+    },
+
+    emailAggregations: function(response, property) {
+      var data = getAggregationDataFromResponse(response, property);
+      return data.map(function(bucket) {
+        var id = ('' + bucket.key).toLowerCase();
+        /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+        return {
+          count: bucket.doc_count,
+          id: id,
+          link: commonTransforms.getLink(bucket.key, 'email')
+        };
+        /* jscs:enable requireCamelCaseOrUpperCaseIdentifiers */
+      });
+    },
+
+    filterAggregations: function(response, property) {
+      var data = getAggregationDataFromResponse(response, property);
+      return data.map(function(bucket) {
+        var id = ('' + bucket.key).toLowerCase();
+        /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+        return {
+          count: bucket.doc_count,
+          id: id
+        };
+        /* jscs:enable requireCamelCaseOrUpperCaseIdentifiers */
+      });
+    },
+
+    phoneAggregations: function(response, property) {
+      var data = getAggregationDataFromResponse(response, property);
+      return data.map(function(bucket) {
+        /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+        return {
+          count: bucket.doc_count,
+          id: bucket.key,
+          link: commonTransforms.getLink(bucket.key, 'phone'),
+          text: commonTransforms.getFormattedPhone(bucket.key)
+        };
+        /* jscs:enable requireCamelCaseOrUpperCaseIdentifiers */
+      });
+    },
+
+    socialMediaAggregations: function(response, property) {
+      var data = getAggregationDataFromResponse(response, property);
+      return data.map(function(bucket) {
+        var id = ('' + bucket.key).toLowerCase();
+
+        /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+        return {
+          count: bucket.doc_count,
+          id: id.substring(id.indexOf(' ') + 1, id.length),
+          link: commonTransforms.getLink(bucket.key, 'social'),
+          text: id
+        };
+        /* jscs:enable requireCamelCaseOrUpperCaseIdentifiers */
+      });
+    },
+
+    // Heights, prices, weights, etc.
+    compoundExtractionAggregations: function(response, property) {
+      var data = getAggregationDataFromResponse(response, property);
+      return data.map(function(bucket) {
+        var extractionData = commonTransforms.getExtractionDataFromCompoundId(bucket.key);
+        /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+        return {
+          count: bucket.doc_count,
+          id: extractionData.id,
+          text: extractionData.text
+        };
+        /* jscs:enable requireCamelCaseOrUpperCaseIdentifiers */
+      }).filter(function(extraction) {
+        return extraction.text;
+      });
     }
   };
 });
