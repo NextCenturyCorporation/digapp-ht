0ff9956f152283a1ff1ff5bb0f6e594ca0b009d3
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2017-06-19 09:51:18 -0400

    Updated the versions of various dependencies and their usage on the entity pages.

--- server/app.js
+++ server/app.js
@@ -34,6 +34,7 @@ require('./routes')(app);
 // Start server
 server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  console.log('Server config', config);
 });
 
 // Expose app
